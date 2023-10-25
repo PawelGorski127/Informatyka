1026cbe21d80a5bd0ad2da8ec090a837cbb7ee46
PawelGorski127 <pgorski547@technikumkreatywne.pl>
2023-10-25 11:51:06 +0200

    Komentarz

--- Lekcja-1/Lekcja 1.cpp
+++ Lekcja-1/Lekcja 1.cpp
@@ -7,8 +7,20 @@
  	
  	
  	
- 	
+ 	 short liczba =2;  //zmiennaliczba
+ 	 float liczbauzytkownika;
  	 cout << "Hello World" << endl;
- cout << "Paweł";
+     cout << "Pawel " << liczba;  //wyswietlanie zmiennej
+     cout << endl;  // koniec lini
+     cout <<"Podaj liczbe: ";
+     cin >> liczbauzytkownika;  //wprowadzanie danych i zapisanie do zmiennej
+     
+     cout << endl;
+     cout << "Podana liczba: " << liczbauzytkownika;
+     
+     /*
+     komentarz
+     na wiele lini
+     */
  	return 0;
  }
