f3f7ee90e9dc655ac79085c212461bf24b27217b
PawelGorski127 <pgorski547@technikumkreatywne.pl>
2023-11-13 15:02:39 +0100

    komentarz

---
+++ Lekcja-1/Lekcja 4.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    cout << "Podaj liczbe: ";
+for (int Wysokosc=0; Wysokosc <= 3; Wysokosc++ ){ // tutaj jest wysokość kwadratu
+if (Wysokosc < 3)
+cout << Wysokosc << "*" << 3*0 << endl;
+}
+	for (int Szerokosc=0; Szerokosc <= 5; Szerokosc++){ // tutaj jest szerokość kwadratu
+if (Szerokosc < 5)
+}
+
+	
+	
+	
+	cout << endl;
+
+}
+	
+	
+
+
+
+
+
+
+
+
+
+
+
