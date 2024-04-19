90feaeef4e75506831693306689ffb42458fdb3d
PawelGorski127 <148331562+PawelGorski127@users.noreply.github.com>
2024-04-19 07:33:11 +0200

    Add files via upload

---
+++ Zad-Dom_Test-(poprawioneleczniedzialawciaz).cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+// Jest tylko jeden błąd, nie wiem gdzie postawić tego "{" żeby to działało
+{
+    int pozycja;
+    int tablica[1];
+    int rozmiar;
+    for (int i = 0; i < rozmiar; ++i) {
+        if (tablica == szukana) {
+            pozycja = i;           
+        }
+    }
+    if (pozycja = 0) {
+        cout << "Poszukiwana liczba znajduje sie na pozycji: " << pozycja << endl;
+    } else {
+        cout << "Nie znaleziono liczby" << endl;
+    }
+}
+int main() {
+    int tablica[] = {2, 3, 5, 7, 11, 2, 3, 5, 7, 11, 31, 37, 41, 43, 47, 2, 3, 5, 7, 11, 31, 37, 41, 43, 47};
+    int rozmiar = sizeof(tablica[1]);
+    int szukana;
+    cout << "Jaka liczbe poszukujesz: ";
+    cin >> szukana;
+    cout << szukana << tablica << rozmiar << endl;
+    return 0;
+}
